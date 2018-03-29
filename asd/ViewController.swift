14e67d53e2af320d3d59a9fec8d6b4346601e781
D7703_18 <d7703_18@d7703-18ui-imac.local>
2018-03-29 12:22:30 +0900

    test

--- asd/ViewController.swift
+++ asd/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func reset(_ sender: Any) {
+        n.text=""
+    }
+    @IBOutlet weak var n: UITextField!
+    @IBOutlet weak var res: UILabel!
+    @IBAction func bt(_ sender: Any) {
+        let num=Int(n.text!)
+        var isp=false
+
+    
+        if num != 1 && num != 2
+        {
+        for i in 2 ..< num!
+        {
+            if num! % i == 0
+            {
+                isp = true
+            }
+        }
+        }
+        
+        if isp == false
+        {
+            res.text="소수"
+        }
+        else
+        {
+            res.text="소수아님"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
